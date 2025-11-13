ce33cdfced1e2b49d84a01809c69bb411060856c
RAFAEL CELIO DA SILVA <rafael.silva37@alunos.sc.senac.br>
2025-11-13 11:02:07 -0300

    primeiro commit

---
+++ src/main/java/com/seubolso/SeubolsoApplication.java
@@ -0,0 +1,13 @@
+package com.seubolso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeubolsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeubolsoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seubolso/SeubolsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seubolso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeubolsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
